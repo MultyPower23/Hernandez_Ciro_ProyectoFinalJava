da31294b4527ace22d71f1a3e37a4e9d7bd8b686
MultyPower23 <juanestebancirogallego9@gmail.com>
2026-09-08 18:30:20 -0500

    Metodo imprimirEncabezado

--- src/aeropuerto/Utilidades.java
+++ src/aeropuerto/Utilidades.java
@@ -1,5 +1,18 @@
 package aeropuerto;
 
+/*
+ * Clase de utilidades generales para el programa. Solo guarda metodos recurrentes que se usan en
+ * varias partes del programa, como imprimir el encabezado con los nombres de los integrantes del
+ * equipo.
+ */
+
 public class Utilidades {
-  
+
+  // Imprime el encabezado con los nombres de los dos integrantes del equipo.
+  public static void imprimirEncabezado() {
+    System.out.println("========================================");
+    System.out.println("Sistema de Gestion Aeroportuaria");
+    System.out.println("Integrantes: Sebastian Hernandez y Juan Esteban Ciro");
+    System.out.println("========================================");
+  }
 }
